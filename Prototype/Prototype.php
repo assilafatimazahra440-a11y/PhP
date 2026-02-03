4c472bba720f98dce113584a48d7850fbc945c4c
fatima <assilafatimazahra440@gmail.com>
2026-02-03 11:29:52 +0000

    upp

--- Prototype/Prototype.php
+++ Prototype/Prototype.php
@@ -30,19 +30,19 @@
     {
      $check=checkRange($notes);
         if($check)
-            return ($notes[0]+ $notes[1]+$notes[2]) / 3;
+            return ($notes[0]+ $notes[1]+$notes[2]) /3;
 }
 
 
 
-function getMention($Avg){
+function getMention($AVG){
      $name=$_POST['nom'];
-    if($Avg <10) return 'ratrapage';
-    if ($Avg<=11) return 'passable';
-    if ($Avg<=14) return 'assez bien ';
-    if ($Avg<=16) return 'bieen';
-    if ($Avg<=18) return 'tres tres bien ';
-    if ($Avg<=20) return 'congratulation  '.$name."     you are genius 🚀 ";
+    if($AVG <10) return 'ratrapage';
+    if ($AVG<=11) return 'passable';
+    if ($AVG<=14) return 'assez bien ';
+    if ($AVG<=16) return ' tres bien';
+    if ($AVG<=18) return 'excelent ';
+    if ($AVG<=20) return 'congratulation  '.$name."     vous etes genius 🚀 ";
 
 
 }
@@ -51,13 +51,13 @@ function getMention($Avg){
 function PrintResult($notes){
      $check=checkRange($notes);
     if( $check){
-    echo"the average is  <br>";
-    echo calculateAVG($notes)."<br>";
-    echo  "the mention is    <br>";
+    echo"le moyenne est    <br>";
+    echo   round(calculateAVG($notes), 2)."<br>";
+    echo  "la mention est    <br>";
     echo getMention(calculateAVG($notes))."<br>";
     }
     else
-         echo "Please enter number between 1 and 20  ";
+         echo "svp entrer un nombre entre 0 et 20  ";
  
 
 }
